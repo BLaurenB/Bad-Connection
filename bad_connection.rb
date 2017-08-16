8777de003702e0a7610193e8390dde22e8716715
BLaurenB <blbillington1@gmail.com>
2017-08-16 13:53:42 -0600

    I was able to get some sections to work, but didn't finish

---
+++ bad_connection.rb
@@ -0,0 +1,47 @@
+
+
+ready_to_quit = false
+input_goodbye = 0
+
+puts "HELLO, THIS IS A GROCERY STORE!"
+
+# input = gets
+#
+# while ready_to_quit = false
+# if input == "\n"  #works
+#   puts "HELLO?"
+#   ready_to_quit = false
+# if input.include?("a..z") # works!
+#   puts "I AM HAVING A HARD TIME HEARING YOU."
+#   ready_to_quit = false
+# if input == input.upcase #aworks!
+#   puts "NO, THIS IS NOT A PET STORE"
+#   ready_to_quit = false
+# input = gets
+# if input = "GOODBYE"
+#   while input_goodbye = 0
+#    puts "ANYTHING ELSE I CAN HELP WITH?"
+#    input_goodbye = input_goodbye + 1
+#  end
+#  puts "THANK YOU FOR CALLING!"
+#  ready_to_quit = true
+
+#   else
+#     puts "WHAT?"
+#   end
+# end
+#
+# puts "THANK YOU FOR CALLING!"
+
+
+#TESTING
+input = gets
+if input = "GOODBYE"
+  while input_goodbye = 0
+   puts "ANYTHING ELSE I CAN HELP WITH?"
+   input_goodbye = input_goodbye + 1
+ end
+ puts "THANK YOU FOR CALLING!"
+ ready_to_quit = true
+
+puts "END"
